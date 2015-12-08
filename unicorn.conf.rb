83bd454d627c86a7d22c95bb2a27a279711781c5
billy3321 <billy3321@gmail.com>
2015-12-08 14:57:37 +0800

    add worker_processes

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -1,4 +1,5 @@
 working_directory ""
+worker_processes 4
 listen "/var/run/unicorn/jrf_digital_unicorn.sock", :backlog => 64
 pid "/var/run/unicorn/jrf_digital_unicorn.pid"
 stderr_path "/var/log/jrf_digital_unicorn.stderr.log"
