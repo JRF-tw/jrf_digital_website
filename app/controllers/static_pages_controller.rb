b50d681eb1a0f455b9337823fd1dc827feb5dbde
billy3321 <billy3321@gmail.com>
2016-05-14 15:11:21 +0800

    fix meta tags

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     @records = Record.includes(:subjects).order(visits: :desc).first(3)
     set_meta_tags({
-      title: '首頁'
+      title: '首頁',
+      og: {
+        title: '首頁'
+      }
     })
   end
 
